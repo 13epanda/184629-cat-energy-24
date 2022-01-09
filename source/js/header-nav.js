c8dbfae9b46d97bb647cf7ec528f5044f455dbf2
Vera Tikhonova <veratihonova95@gmail.com>
2022-01-09 16:55:59 +0500

    вывод js header-nav в отдельный файл и добавление ко всем страницам

---
+++ source/js/header-nav.js
@@ -0,0 +1,11 @@
+var navMain = document.querySelector('.page-header__wrapper-nav');
+var navToggle = document.querySelector('.page-header__toggle');
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('page-header--closed')) {
+  navMain.classList.remove('page-header--closed');
+  navMain.classList.add('page-header--opened');
+  } else {
+    navMain.classList.add('page-header--closed');
+    navMain.classList.remove('page-header--opened');
+  }
+});
