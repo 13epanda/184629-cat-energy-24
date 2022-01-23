8e9a30263a6d4b780b9acf2e21ec2fc9bc68917c
Vera Tikhonova <veratihonova95@gmail.com>
2022-01-23 14:51:13 +0500

    added sprite

--- gulpfile.js
+++ gulpfile.js
@@ -69,7 +69,7 @@ const createWebp = () => {
 // SVG
 
 const svg = () =>
-  gulp.src(['source/img/**/*.svg', '!source/img/sprite/*.svg'])
+  gulp.src(['source/img/**/*.svg','!source/img/sprite.svg', '!source/img/sprite/*.svg'])
     .pipe(svgo())
     .pipe(gulp.dest('build/img'));
 
