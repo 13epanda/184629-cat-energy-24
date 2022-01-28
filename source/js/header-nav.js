a338190f1c6286796d41cb0f6090d7ac941e9658
Vera Tikhonova <veratihonova95@gmail.com>
2022-01-28 21:47:49 +0500

    Б5. remove page-header--closed on wrapper-nav

--- source/js/header-nav.js
+++ source/js/header-nav.js
@@ -1,11 +1,11 @@
 var navMain = document.querySelector('.page-header__wrapper-nav');
 var navToggle = document.querySelector('.page-header__toggle');
 navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('page-header--closed')) {
-  navMain.classList.remove('page-header--closed');
+  if (navMain.classList.contains('page-header__wrapper-nav--closed')) {
+  navMain.classList.remove('page-header__wrapper-nav--closed');
   navMain.classList.add('page-header--opened');
   } else {
-    navMain.classList.add('page-header--closed');
+    navMain.classList.add('page-header__wrapper-nav--closed');
     navMain.classList.remove('page-header--opened');
   }
 });
