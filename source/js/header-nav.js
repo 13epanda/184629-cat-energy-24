4f210c5bbe3017ff7974a32bcb2b26bca9f7bbfa
Vera Tikhonova <veratihonova95@gmail.com>
2022-01-29 23:19:05 +0500

    Б30. fixed mobile menu

--- source/js/header-nav.js
+++ source/js/header-nav.js
@@ -1,5 +1,9 @@
 var navMain = document.querySelector('.page-header__wrapper-nav');
 var navToggle = document.querySelector('.page-header__toggle');
+
+//Скрытие меню, видимое по умолчанию
+navMain.classList.add('page-header__wrapper-nav--closed');
+
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('page-header__wrapper-nav--closed')) {
   navMain.classList.remove('page-header__wrapper-nav--closed');
